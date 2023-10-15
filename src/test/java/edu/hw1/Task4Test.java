160cd66fe8f7146924f65e2601403b84f6e838cf
Alex112323 <aa69128210@gmail.com>
2023-10-15 22:51:44 +0400

    changes added 3

--- src/test/java/edu/hw1/Task4Test.java
+++ src/test/java/edu/hw1/Task4Test.java
@@ -6,39 +6,38 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Task4Test {
 
-    @Test
-    void fixString_1() {
+    @Test void fixString_1() {
         Task4 task4 = new Task4();
         String string = "123456";
         String response = task4.fixString(string);
-        assertEquals( "214365", response);
+        assertEquals("214365", response);
     }
-    @Test
-    void fixString_2() {
+
+    @Test void fixString_2() {
         Task4 task4 = new Task4();
         String string = "hTsii  s aimex dpus rtni.g";
         String response = task4.fixString(string);
-        assertEquals( "This is a mixed up string.", response);
+        assertEquals("This is a mixed up string.", response);
     }
-    @Test
-    void fixString_3() {
+
+    @Test void fixString_3() {
         Task4 task4 = new Task4();
         String string = "badce";
         String response = task4.fixString(string);
-        assertEquals( "abcde", response);
+        assertEquals("abcde", response);
     }
-    @Test
-    void fixString_4() {
+
+    @Test void fixString_4() {
         Task4 task4 = new Task4();
         String string = "b";
         String response = task4.fixString(string);
-        assertEquals( "b", response);
+        assertEquals("b", response);
     }
-    @Test
-    void fixString_5() {
+
+    @Test void fixString_5() {
         Task4 task4 = new Task4();
         String string = "";
         String response = task4.fixString(string);
-        assertEquals( "", response);
+        assertEquals("", response);
     }
 }
