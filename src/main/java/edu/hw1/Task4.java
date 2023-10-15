ad83e559cf2b8c92dd6feea0363fd15f2e6ce327
Alex112323 <aa69128210@gmail.com>
2023-10-15 22:41:16 +0400

    changes added 2

--- src/main/java/edu/hw1/Task4.java
+++ src/main/java/edu/hw1/Task4.java
@@ -7,8 +7,7 @@ public class Task4 {
             return string;
         }
         for (int i = 0; i < string.length() - 1 - string.length() % 2; i += 2) {
-            response += string.charAt(i + 1);
-            response += string.charAt(i);
+            response = response + string.charAt(i + 1) + string.charAt(i);
         }
         if (string.length() % 2 == 1) {
             response += string.charAt(string.length() - 1);
