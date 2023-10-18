e7d1220e7d23759ac1e50ce55c6e2a11ffe1042e
Alex112323 <aa69128210@gmail.com>
2023-10-18 19:24:41 +0400

    changes added 5

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,24 +1,23 @@
-package edu.hw1;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SampleTest {
     @Test
     @DisplayName("Фильтрация четных чисел")
     void filterEvenNumbers() {
         // given
-        int[] numbers = new int[] {1, 2, 3, 4, 5};
+        int[] numbers = new int[]{1, 2, 3, 4, 5};
 
         // when
-        int[] evenNumbers = EvenArrayUtils.filter(numbers);
+        int[] evenNumbers = evenNumbers(numbers);
 
         // then
         assertThat(evenNumbers)
             .containsExactly(2, 4)
             .hasSize(2);
     }
+
     @Test
     void failedTest() {
         assertThat(5).isEqualTo(2 * 2);
@@ -47,4 +46,3 @@ public class SampleTest {
         return count;
     }
 }
-
