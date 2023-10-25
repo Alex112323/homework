95c01817cc31a9898a29847e5eddf31cfe1ad12f
Alex112323 <aa69128210@gmail.com>
2023-10-25 14:08:32 +0400

    Add changes 8

--- src/main/java/edu/hw2/Task1/Expr.java
+++
@@ -1,5 +0,0 @@
-package edu.hw2.Task1;
-
-public sealed interface Expr permits Constant, Addition, Negate, Exponent, Multiplication {
-    double evaluate();
-}
--- src/main/java/edu/hw2/Task4/CallingInfo.java
+++
@@ -1,9 +0,0 @@
-package edu.hw2.Task4;
-
-public record CallingInfo(String className, String methodName) {
-    public static CallingInfo callingInfo() {
-        Throwable exception = new Throwable();
-        StackTraceElement info = exception.getStackTrace()[1];
-        return new CallingInfo(info.getClassName(), info.getMethodName());
-    }
-}
