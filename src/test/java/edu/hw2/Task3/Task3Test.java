47b890ce355d4693dfee944b0b9528166cc88669
Alex112323 <aa69128210@gmail.com>
2023-10-25 13:47:06 +0400

    Add changes 8

--- src/test/java/edu/hw2/Task3/Task3Test.java
+++ src/test/java/edu/hw2/Task3/Task3Test.java
@@ -7,26 +7,12 @@ public class Task3Test {
     @Test
     void get_Stable_Connection() {
         PopularCommandExecutor commandExecutore = new PopularCommandExecutor(new DefaultConnectionManager(), 10);
-        boolean executed;
-        try {
-            commandExecutore.updatePackages();
-            executed = true;
-        } catch (ConnectionException exception) {
-            executed = false;
-        }
-        assertTrue(executed);
+        assertDoesNotThrow(() -> commandExecutore.updatePackages());
     }
     @Test
-    void GetConnection() {
+    void get_Connection() {
         PopularCommandExecutor executor =
             new PopularCommandExecutor(new FaultyConnectionManager(), 20);
-        boolean executed;
-        try {
-            executor.updatePackages();
-            executed = true;
-        } catch (ConnectionException exception) {
-            executed = false;
-        }
-        assertTrue(executed);
+        assertDoesNotThrow(() -> executor.updatePackages());
     }
 }
