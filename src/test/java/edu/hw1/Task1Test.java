85fc027d78f8f7c308af226569bac5b87df42d13
Alex112323 <aa69128210@gmail.com>
2023-10-29 01:14:03 +0400

    Add changes 9

---
+++ src/test/java/edu/hw1/Task1Test.java
@@ -0,0 +1,23 @@
+package edu.hw1;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task1Test {
+
+    @ParameterizedTest
+    @CsvSource(value = {
+        "01:00, 60",
+        "13:56, 836",
+        "10:60, -1",
+        ":30, -1",
+        "003:20, 200",
+        "1234, -1"
+    })
+    void minutes_To_Seconds(String string, int correctAnswer) {
+        Task1 task1 = new Task1();
+        int response = task1.minutesToSeconds(string);
+        assertEquals(correctAnswer, response);
+    }
+}
